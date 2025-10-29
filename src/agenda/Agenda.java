ac2935fdc5714ece14c4aced44ebfde9292d4fb8
matias juncal <130884867+mavancal@users.noreply.github.com>
2025-10-28 23:04:41 -0300

    Initial commit

---
+++ src/agenda/Agenda.java
@@ -0,0 +1,14 @@
+package agenda;
+
+import agenda.gui.Principal;
+
+public class Agenda {
+
+    public static void main(String[] args) {
+        
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+    
+}
